06ff0c8a0bcc8ab9a1c0b8d36c4aaa94cd086888
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-04-30 18:16:44 +0300

    edit navbar

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { AlignJustify, LogOut } from "lucide-react";
 
 const AdminHeader = () => {
-  return <div>AdminHeader</div>;
+  return (
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button className="lg:hidden ">
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end ">
+        <Button>
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default AdminHeader;
